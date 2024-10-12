d4b491a2ac67070ee233bb71330e2e7de7f6cc82
Chaozhong Liu <czliubioinfo@gmail.com>
2024-10-12 00:28:46 -0500

    Bubble Manager init

---
+++ DyberPet/bubbleManager.py
@@ -0,0 +1,36 @@
+
+from PySide6.QtCore import QObject
+
+
+class BubbleManager(QObject):
+    """
+    Class to manage all behaviors of bubbleText
+    """
+
+    def __init__(self,
+                 parent=None):
+        super().__init__(parent=parent)
+
+
+    def trigger_patpat(self):
+        return
+    
+    def trigger_scheduled(self):
+        return
+    
+    def _trigger_HP(self):
+        return
+    
+    def _trigger_FV(self):
+        return
+    
+    def _trigger_feed(self):
+        return
+    
+    def _trigger_focus(self):
+        return
+
+
+
+
+
